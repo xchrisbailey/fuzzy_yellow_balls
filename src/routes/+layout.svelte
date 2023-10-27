8ca8a9a49886bef12ca57853ebde78956f280d0b
Chris Bailey <chris@ghostly.io>
2023-10-27 13:05:23 -0400

    feat(userButton): ✨ add user button drop down when logged in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import { Button } from '$lib/components/ui/button';
+	import UserButton from '$lib/components/user_button.svelte';
 	import '../app.postcss';
 	import type { PageData } from './$types';
-	import { LogOut, LogIn } from 'lucide-svelte';
+	import { LogIn } from 'lucide-svelte';
 
 	export let data: PageData;
+
+	console.log(data);
 </script>
 
 <header class="container flex justify-between my-3 mx-auto">
@@ -16,12 +18,7 @@
 			login</Button
 		>
 	{:else}
-		<form method="post" action="/logout?" use:enhance>
-			<Button variant="destructive">
-				<LogOut class="mr-2 w-4 h-4" />
-				logout
-			</Button>
-		</form>
+		<UserButton image={data?.session?.user?.image} />
 	{/if}
 </header>
 
