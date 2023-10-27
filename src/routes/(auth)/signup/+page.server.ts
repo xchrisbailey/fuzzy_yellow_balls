7d54678c0f84f17c5bf9bf8dde9e2c95d80a9acf
Chris Bailey <chris@ghostly.io>
2023-10-27 10:24:10 -0400

    feat(auth): ✨ add user signup and prisma migration adding email

---
+++ src/routes/(auth)/signup/+page.server.ts
@@ -0,0 +1,56 @@
+import { auth } from '$lib/server/auth';
+import { fail, redirect } from '@sveltejs/kit';
+
+import type { PageServerLoad, Actions } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const session = await locals.auth.validate();
+	if (session) {
+		throw redirect(302, '/');
+	}
+	return {};
+};
+
+export const actions: Actions = {
+	default: async ({ request, locals }) => {
+		const formData = await request.formData();
+		const email = formData.get('email');
+		const password = formData.get('password');
+
+		if (typeof email !== 'string' || email.length < 1 || email.length > 255) {
+			return fail(400, {
+				message: 'Invalid email'
+			});
+		}
+
+		if (typeof password !== 'string' || password.length < 6 || password.length > 255) {
+			return fail(400, {
+				message: 'Invalid password'
+			});
+		}
+		try {
+			const user = await auth.createUser({
+				key: {
+					providerId: 'email', // auth method
+					providerUserId: email.toLowerCase(), // unique id when using "email" auth method
+					password // hashed by Lucia
+				},
+				attributes: {
+					email: email.toLowerCase()
+				}
+			});
+			const session = await auth.createSession({
+				userId: user.userId,
+				attributes: {}
+			});
+			locals.auth.setSession(session); // set session cookie
+		} catch (e) {
+			console.log(e);
+
+			return fail(500, {
+				message: 'An unknown error occurred'
+			});
+		}
+		throw redirect(302, '/');
+	}
+};
