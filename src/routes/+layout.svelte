09d30ebaa81dbd94c090b9cbe0ee2cd31c6ca75c
Chris Bailey <chris@ghostly.io>
2023-10-27 11:41:59 -0400

    feat(auth): ✨ login/logout button in header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
-<script>
+<script lang="ts">
 	import { enhance } from '$app/forms';
 	import '../app.postcss';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
 <header>
-	<form method="post" action="/logout?" use:enhance>
-		<button>logout</button>
-	</form>
+	{#if !data.session?.user}
+		<a href="/login">login</a>
+	{:else}
+		<form method="post" action="/logout?" use:enhance>
+			<button>logout</button>
+		</form>
+	{/if}
 </header>
 <slot />
-
