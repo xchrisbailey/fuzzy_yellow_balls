08b41bcdc9cb4ba10f163e1da7b8bd5bacd12d01
Chris Bailey <chris@ghostly.io>
2023-10-30 06:57:57 -0400

    style(prettier): 🎨 tailwind prettier plugin

--- src/routes/(protected)/profile/+page.svelte
+++ src/routes/(protected)/profile/+page.svelte
@@ -8,3 +8,20 @@
 <h1 class="text-4xl font-extrabold tracking-tight lg:text-5xl scroll-m-20 text-primary">
 	Hello {title_case(data.session.user.first_name)}
 </h1>
+
+<section class="grid grid-cols-2 gap-5">
+	<article>
+		<h2
+			class="pb-2 text-3xl font-semibold tracking-tight border-b transition-colors first:mt-0 scroll-m-20"
+		>
+			Your reviews
+		</h2>
+	</article>
+	<article>
+		<h2
+			class="pb-2 text-3xl font-semibold tracking-tight border-b transition-colors first:mt-0 scroll-m-20"
+		>
+			Edit Your Profile
+		</h2>
+	</article>
+</section>
