73a57ae9414eb292e76daa08359bd511e845ae09
Chris Bailey <chris@ghostly.io>
2023-11-14 19:26:40 -0500

    feat(review): ✨ add flash message and error when user has already reviewed a string

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
 import type { LayoutServerLoad } from './$types';
+import { loadFlash } from 'sveltekit-flash-message/server';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = loadFlash(async ({ locals }) => {
 	const session = await locals.auth.validate();
 	return { session };
-};
+});
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,13 +5,29 @@
 	import type { PageData } from './$types';
 	import { LogIn } from 'lucide-svelte';
 	import { Toaster } from 'svelte-french-toast';
+	import { getFlash } from 'sveltekit-flash-message';
+	import { page } from '$app/stores';
+	import * as Alert from '$lib/components/ui/alert';
+
+	const flash = getFlash(page, {
+		clearAfterMs: 4000
+	});
 
 	export let data: PageData;
 </script>
 
 <Toaster />
+{#if $flash}
+	<Alert.Root
+		variant={$flash.message === 'error' ? 'destructive' : 'default'}
+		class="absolute right-5 bottom-5 w-1/3 opacity-75 bg-destructive text-destructive-foreground"
+	>
+		<Alert.Title>{$flash.type === 'error' ? 'Uh Oh' : 'Yippy'}</Alert.Title>
+		<Alert.Description>{$flash.message}</Alert.Description>
+	</Alert.Root>
+{/if}
 
-<header class="container mx-auto my-3 flex justify-between">
+<header class="container flex justify-between my-3 mx-auto">
 	<div class="text-3xl font-bold">The String Bar</div>
 	<nav class="flex items-center">
 		<ul class="flex gap-5">
@@ -21,7 +37,7 @@
 	</nav>
 	{#if !data.session?.user}
 		<Button href="/login">
-			<LogIn class="mr-2 h-4 w-4" />
+			<LogIn class="mr-2 w-4 h-4" />
 			login</Button
 		>
 	{:else}
@@ -29,6 +45,6 @@
 	{/if}
 </header>
 
-<main class="container mx-auto my-5">
+<main class="container my-5 mx-auto">
 	<slot />
 </main>
