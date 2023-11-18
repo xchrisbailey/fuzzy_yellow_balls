c74168c82b098b5cf84f02e8c71c08ea9592d5ca
Chris Bailey <chris@ghostly.io>
2023-11-18 16:28:46 -0500

    feat(header): ✨ add menu icon on small screen

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -2,18 +2,23 @@
 	import { AppBar } from '@skeletonlabs/skeleton';
 	import type { Session } from 'lucia';
 	import UserButton from './user_button.svelte';
-	import { LogIn } from 'lucide-svelte';
+	import { LogIn, Menu } from 'lucide-svelte';
 
 	export let session: Session | null;
 </script>
 
 <AppBar
-	gridColumns="grid-cols-2"
-	slotDefault="place-content-start"
+	gridColumns="md:grid-cols-2 grid-cols-3"
+	slotLead="md:hidden"
+	slotDefault="md:place-content-start place-content-center"
 	slotTrail="place-content-end"
 	background="bg-transparent"
 >
-	<div class="text-3xl font-bold">The String Bar</div>
+	<svelte:fragment slot="lead"><Menu /></svelte:fragment>
+
+	<div class="font-bold leading-relaxed lowercase gradient-heading-pink h2 drop-shadow-sm">
+		The String Bar
+	</div>
 	<svelte:fragment slot="trail">
 		{#if !session?.user}
 			<a href="/login" class="variant-soft-primary btn">
