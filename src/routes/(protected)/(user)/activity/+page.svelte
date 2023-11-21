44326dec933fa23b3798b1665d02e88b9397636e
Chris Bailey <chris@ghostly.io>
2023-11-21 03:31:02 -0500

    refactor(review): ♻️ extract review card into component

--- src/routes/(protected)/(user)/activity/+page.svelte
+++ src/routes/(protected)/(user)/activity/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Star, Pencil } from 'lucide-svelte';
 	import type { PageData } from './$types';
+	import ReviewCard from '$lib/components/review_card.svelte';
 
 	export let data: PageData;
 </script>
@@ -11,53 +12,7 @@
 		<p class="text-warning-500">you have not reviews anything yet!</p>
 	{:else}
 		{#each data.reviews as review}
-			<article class="p-4 card variant-glass">
-				<p>{review.comments}</p>
-				<div class="grid gap-1 mt-1 md:grid-cols-2 gird-cols-1">
-					<div class="flex gap-1 items-center">
-						comfort: {#each Array(review.comfort) as id}
-							<Star {id} class="w-4 h-4" />
-						{/each}
-					</div>
-					<div class="flex gap-2 items-center">
-						power: {#each Array(review.power) as id}
-							<Star {id} class="w-4 h-4" />
-						{/each}
-					</div>
-					<div class="flex gap-2 items-center">
-						feel: {#each Array(review.feel) as id}
-							<Star {id} class="w-4 h-4" />
-						{/each}
-					</div>
-					<div class="flex gap-2 items-center">
-						control: {#each Array(review.control) as id}
-							<Star {id} class="w-4 h-4" />
-						{/each}
-					</div>
-					<div class="flex gap-2 items-center">
-						durability: {#each Array(review.durability) as id}
-							<Star {id} class="w-4 h-4" />
-						{/each}
-					</div>
-					<div class="flex gap-2 items-center">
-						spin: {#each Array(review.spin) as id}
-							<Star {id} class="w-4 h-4" />
-						{/each}
-					</div>
-					<div class="flex gap-2 items-center">
-						playability: {#each Array(review.playability) as id}
-							<Star {id} class="w-4 h-4" />
-						{/each}
-					</div>
-				</div>
-				{#if review.user_id === data.session?.user.userId}
-					<div class="flex justify-end">
-						<a href="/reviews/{review.id}/update" class="variant-ghost-primary btn-icon btn-icon-sm"
-							><Pencil class="w-4 h-4" /></a
-						>
-					</div>
-				{/if}
-			</article>
+			<ReviewCard user_id={data.session?.user.userId} {review} />
 		{/each}
 	{/if}
 </section>
--- src/routes/(public)/strings/[id]/+page.svelte
+++ src/routes/(public)/strings/[id]/+page.svelte
@@ -2,84 +2,38 @@
 	import { title_case } from '$lib/helpers/title_case';
 	import { htmlUnescape } from 'escape-goat';
 	import type { PageData } from './$types';
-	import { Pencil, Star } from 'lucide-svelte';
+	import { Pencil } from 'lucide-svelte';
+	import ReviewCard from '$lib/components/review_card.svelte';
 
 	export let data: PageData;
 </script>
 
-<h1 class="mb-4 leading-relaxed h1 gradient-heading-pink">
+<h1 class="mb-4 leading-relaxed gradient-heading-pink h1">
 	{title_case(data.string.name)} by {title_case(data.string.Brand.name)}
 </h1>
 <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
 	<section>
 		<article>
-			<h2 class="h2 gradient-heading-blue drop-shadow-sm">Info</h2>
+			<h2 class="gradient-heading-blue h2 drop-shadow-sm">Info</h2>
 			<p class="py-2 whitespace-pre-line">{data.string.description}</p>
 		</article>
 	</section>
 	<section>
 		<div class="mb-4">
-			<h2 class="mb-2 h2 gradient-heading-blue drop-shadow-sm">User Reviews</h2>
+			<h2 class="mb-2 gradient-heading-blue h2 drop-shadow-sm">User Reviews</h2>
 			{#each data.string.Review as review}
-				<article class="p-4 card variant-glass">
-					<p>{review.comments}</p>
-					<div class="grid gap-1 mt-1 md:grid-cols-2 gird-cols-1">
-						<div class="flex gap-1 items-center">
-							comfort: {#each Array(review.comfort) as id}
-								<Star {id} class="w-4 h-4" />
-							{/each}
-						</div>
-						<div class="flex gap-2 items-center">
-							power: {#each Array(review.power) as id}
-								<Star {id} class="w-4 h-4" />
-							{/each}
-						</div>
-						<div class="flex gap-2 items-center">
-							feel: {#each Array(review.feel) as id}
-								<Star {id} class="w-4 h-4" />
-							{/each}
-						</div>
-						<div class="flex gap-2 items-center">
-							control: {#each Array(review.control) as id}
-								<Star {id} class="w-4 h-4" />
-							{/each}
-						</div>
-						<div class="flex gap-2 items-center">
-							durability: {#each Array(review.durability) as id}
-								<Star {id} class="w-4 h-4" />
-							{/each}
-						</div>
-						<div class="flex gap-2 items-center">
-							spin: {#each Array(review.spin) as id}
-								<Star {id} class="w-4 h-4" />
-							{/each}
-						</div>
-						<div class="flex gap-2 items-center">
-							playability: {#each Array(review.playability) as id}
-								<Star {id} class="w-4 h-4" />
-							{/each}
-						</div>
-					</div>
-					{#if review.user_id === data.session?.user.userId}
-						<div class="flex justify-end">
-							<a
-								href="/reviews/{review.id}/update"
-								class="btn-icon btn-icon-sm variant-ghost-primary"><Pencil class="w-4 h-4" /></a
-							>
-						</div>
-					{/if}
-				</article>
+				<ReviewCard user_id={data.session?.user.userId} {review} />
 			{/each}
 
 			<div class="flex justify-center mt-3">
-				<a href="/reviews/add/{data.string.id.trimEnd()}" class="btn variant-glass-primary"
+				<a href="/reviews/add/{data.string.id.trimEnd()}" class="variant-glass-primary btn"
 					><Pencil class="mr-2 w-4 h-4" />Add Review</a
 				>
 			</div>
 		</div>
 		<hr class="my-5 divide-gray-50" />
 		<div>
-			<h2 class="mb-2 h2 gradient-heading-blue drop-shadow-sm">YouTube Reviews</h2>
+			<h2 class="mb-2 gradient-heading-blue h2 drop-shadow-sm">YouTube Reviews</h2>
 			<div class="space-y-4">
 				{#each data.youtube_reviews.items as youtube_review}
 					<article>
