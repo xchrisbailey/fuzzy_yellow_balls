f7b6c3e0b1aabe912dc6f69ec1ac77ced205ba59
Chris Bailey <chris@ghostly.io>
2023-12-02 10:45:06 -0500

    feat(rackets): ✨ add rackets navigation button

--- src/lib/components/navigation_links.svelte
+++ src/lib/components/navigation_links.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
-	import { Book, Home } from 'lucide-svelte';
+	import { Book, BookHeart, Home } from 'lucide-svelte';
 
 	const drawerStore = getDrawerStore();
 	function drawerClose() {
@@ -11,19 +11,25 @@
 	$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-primary-500' : '');
 </script>
 
-<nav class="list-nav p-3 md:p-0">
+<nav class="p-3 md:p-0 list-nav">
 	<ul>
 		<li>
 			<a href="/" on:click={drawerClose} class={classesActive('/')}>
-				<span class="badge bg-primary-300"><Home class="h-4 w-4" /></span>
+				<span class="badge bg-primary-300"><Home class="w-4 h-4" /></span>
 				<span class="flex-auto">home</span>
 			</a>
 		</li>
 		<li>
 			<a href="/strings" on:click={drawerClose} class={classesActive('/strings')}>
-				<span class="badge bg-success-300"><Book class="h-4 w-4" /></span>
+				<span class="badge bg-success-300"><Book class="w-4 h-4" /></span>
 				<span class="flex-auto">strings</span></a
 			>
+		</li>
+		<li>
+			<a href="/" on:click={drawerClose} class={classesActive('/rackets')}
+				><span class="badge bg-secondary-300"><BookHeart class="w-4 h-4" /></span>
+				<span class="flex-auto">rackets</span>
+			</a>
 		</li>
 	</ul>
 </nav>
