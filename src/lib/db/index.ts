66f72f16600dd160d88eea686e8e3878eed31028
Chris Bailey <chris@ghostly.io>
2023-12-03 00:09:47 -0500

    refactor(auth): ♻️ lucia postgres adapter

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
+import * as schema from './schema';
 
 // for migrations
 const migrationClient = postgres('postgres://postgres:adminadmin@0.0.0.0:5432/db', { max: 1 });
@@ -9,5 +10,5 @@ migrate(drizzle(migrationClient), {
 });
 
 // for query purposes
-const queryClient = postgres('postgres://postgres:postgres@0.0.0.0:5432/string_bar');
-export const db = drizzle(queryClient);
+export const queryClient = postgres('postgres://postgres:postgres@0.0.0.0:5432/string_bar');
+export const db = drizzle(queryClient, { schema });
