c0c5dacf317fa1bf9d533561fb88d23c51cff57c
Chris Bailey <chris@ghostly.io>
2023-12-03 08:53:57 -0500

    refactor(strings): ♻️ complete strings move from prisma to drizzle

--- src/routes/(public)/strings/+page.server.ts
+++ src/routes/(public)/strings/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const strings = await locals.db.query.strings.findMany({ with: { brand: true } });
+	const strings = await locals.db.query.strings.findMany({ with: { brand: true, reviews: true } });
 	return {
 		strings
 	};
