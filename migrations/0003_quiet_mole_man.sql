04d843df2d96dd1552a69f117f4bce2d27533949
Chris <chris@ghostly.io>
2023-12-04 14:14:05 -0500

    feat(user): ✨ not null on schema fields

---
+++ migrations/0003_quiet_mole_man.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "user" ALTER COLUMN "email" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "first_name" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "last_name" SET NOT NULL;
