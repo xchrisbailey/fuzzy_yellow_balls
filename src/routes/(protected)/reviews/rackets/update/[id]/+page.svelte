0ad0b790d7e55d4f22ee678a514ca1ab1d28a399
Chris <chris@ghostly.io>
2023-12-10 16:12:13 -0500

    feat(reviews): ✨ racket review view page

---
+++ src/routes/(protected)/reviews/rackets/update/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import RacketReviewForm from '$lib/components/forms/racket_review_form.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<RacketReviewForm data={data.form} />
