0f8abaa23770f20b52300c6ef53ceecbe11d0080
Chris <chris@ghostly.io>
2023-12-21 03:10:28 -0500

    test(start-rating): 🚨 test star rating component renders

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./setupTest.ts'],
+		coverage: {
+			exclude: ['setupTest.ts']
+		},
+		exclude: [...configDefaults.exclude, 'tests']
 	}
 });
