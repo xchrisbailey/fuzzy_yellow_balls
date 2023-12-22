a1d056524401dde3aee8ddb5096a34b7e37aa364
Chris <chris@ghostly.io>
2023-12-21 23:59:41 -0500

    test(auth): 🚨 login e2e tests

--- tests/e2e/auth.test.ts
+++ tests/e2e/auth.test.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 const USER = {
 	email: 'test@example.com',
@@ -9,8 +9,27 @@ const USER = {
 
 test.describe('user authentication', () => {
 	test.describe('logging in', () => {
-		test('should be able to login', async ({ page }) => {});
-		test('should not be able to login with invalid credentials', async ({ page }) => {});
+		test('should be able to login', async ({ page }) => {
+			await page.goto('/login');
+			await page.locator('input[name="email"]').fill(USER.email);
+			await page.locator('input[name="password"]').fill(USER.password);
+			await page.getByRole('button', { name: 'Log In' }).click();
+			await page.waitForURL('/');
+
+			await page.goto('/profile');
+
+			expect(page.getByRole('heading', { name: 'Hello Test' })).toBeVisible();
+		});
+
+		test('should not be able to login with invalid credentials', async ({ page }) => {
+			await page.goto('/login');
+			await page.locator('input[name="email"]').fill('wrong');
+			await page.locator('input[name="password"]').fill('wronggggg');
+			await page.getByRole('button', { name: 'Log In' }).click();
+
+			await page.waitForLoadState('networkidle');
+			expect(page.getByText('Invalid email')).toBeVisible();
+		});
 	});
 
 	test.describe('registering', () => {
