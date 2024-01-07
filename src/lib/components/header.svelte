d3bddac14b05b1b882a9094d72e93e036e316bad
Chris <chris@ghostly.io>
2024-01-07 03:21:40 -0500

    refactor(header): ♻️ base header and brand

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import type { Session } from 'lucia';
-	import UserButton from './user_button.svelte';
+	// import UserButton from './user_button.svelte';
 	import { LogIn } from 'lucide-svelte';
 
 	export let session: Session | null;
 </script>
 
 <header>
-	<div>Fuzzy Yellow Balls</div>
+	<div class="brand">Fuzzy Yellow Balls</div>
 	{#if !session?.user}
 		<a href="/login">
 			<LogIn />
@@ -17,3 +17,22 @@
 		<!-- <UserButton user={session.user} /> -->
 	{/if}
 </header>
+
+<style>
+	header {
+		display: flex;
+		height: 4rem;
+		background: var(--black);
+		justify-content: space-between;
+		align-items: center;
+		padding: 0.5em;
+	}
+	.brand {
+		font-size: var(--font-lg);
+		font-weight: 800;
+		color: var(--primary-light);
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		margin-left: 0.25em;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,11 @@
 	import './styles.css';
 	import type { PageData } from './$types';
 	import Header from '$lib/components/header.svelte';
-	import NavigationLinks from '$lib/components/navigation_links.svelte';
 
 	export let data: PageData;
 </script>
 
-<NavigationLinks />
 <Header session={data.session} />
-<NavigationLinks />
 <main class="p-5">
 	<slot />
 </main>
