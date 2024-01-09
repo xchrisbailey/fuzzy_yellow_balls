fa7e6249523dd96bde73f6b1cd3645ebc6adc37b
Chris <chris@ghostly.io>
2024-01-09 01:50:14 -0500

    feat(styles): ✨ header navigation base

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Session } from 'lucia';
-	// import UserButton from './user_button.svelte';
+	import { Book, BookHeart, Home } from 'lucide-svelte';
 	import { LogIn } from 'lucide-svelte';
 
 	export let session: Session | null;
@@ -8,31 +8,68 @@
 
 <header>
 	<div class="brand">Fuzzy Yellow Balls</div>
-	{#if !session?.user}
-		<a href="/login">
-			<LogIn />
-			login</a
-		>
-	{:else}
-		<!-- <UserButton user={session.user} /> -->
-	{/if}
+	<div>
+		{#if !session?.user}
+			<a href="/login"><LogIn /> login</a>
+		{:else}
+			<nav>
+				<ul>
+					<li><a href="/"><Home /> home</a></li>
+					<li><a href="/strings"><Book /> strings</a></li>
+					<li><a href="/rackets"><BookHeart /> rackets</a></li>
+				</ul>
+			</nav>
+			<!-- <UserButton user={session.user} /> -->
+		{/if}
+	</div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		height: 4rem;
-		background: var(--black);
+		background: var(--green-400);
 		justify-content: space-between;
 		align-items: center;
 		padding: 0.5em;
+		margin-bottom: 1em;
 	}
+
+	nav {
+		font-size: var(--font-sm);
+		margin-right: 1em;
+		& ul {
+			display: flex;
+			gap: 2em;
+			& li {
+				display: flex;
+				align-items: center;
+				gap: 0.2em;
+				& svg {
+					width: 15px;
+					height: 15px;
+					margin-right: 0.25em;
+				}
+			}
+		}
+		& a {
+			color: var(--black);
+			transition: color 0.2s ease-in-out;
+			&:hover {
+				color: var(--green-700);
+				text-decoration: none;
+			}
+		}
+	}
+
 	.brand {
 		font-size: var(--font-lg);
-		font-weight: 800;
-		color: var(--primary-light);
+		font-variation-settings:
+			'wght' 700,
+			'slnt' -3;
+		color: var(--primary-dark);
 		text-transform: uppercase;
-		letter-spacing: 0.1em;
+		letter-spacing: 0.05em;
 		margin-left: 0.25em;
 	}
 </style>
