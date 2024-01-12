5c962fb7a654c32c1e9628c1fc4d7a0bfd9c43e7
Chris <chris@ghostly.io>
2024-01-12 03:29:23 -0500

    feat(header): ✨ Update dependencies and fix header styling

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -8,20 +8,20 @@
 
 <header>
 	<div class="brand">Fuzzy Yellow Balls</div>
-	<div>
-		{#if !session?.user}
+	{#if !session?.user}
+		<div>
 			<a href="/login"><LogIn /> login</a>
-		{:else}
-			<nav>
-				<ul>
-					<li><a href="/"><Home /> home</a></li>
-					<li><a href="/strings"><Book /> strings</a></li>
-					<li><a href="/rackets"><BookHeart /> rackets</a></li>
-				</ul>
-			</nav>
-			<!-- <UserButton user={session.user} /> -->
-		{/if}
-	</div>
+		</div>
+	{:else}
+		<nav>
+			<ul>
+				<li><a href="/"><Home /> home</a></li>
+				<li><a href="/strings"><Book /> strings</a></li>
+				<li><a href="/rackets"><BookHeart /> rackets</a></li>
+			</ul>
+		</nav>
+		<!-- <UserButton user={session.user} /> -->
+	{/if}
 </header>
 
 <style>
@@ -30,7 +30,7 @@
 		height: 4rem;
 		justify-content: space-between;
 		align-items: center;
-		padding: 0.5em;
+		padding: 0 0.5em;
 		margin-bottom: 1em;
 		margin: 0px 1.5em 1em 1.5em;
 		border-bottom: 1px solid var(--gray-50);
@@ -38,14 +38,21 @@
 
 	nav {
 		font-size: var(--font-sm);
-		margin-right: 1em;
+		height: 100%;
+		display: flex;
+		align-items: center;
 		& ul {
+			height: 100%;
 			display: flex;
-			gap: 2em;
+			align-items: stretch;
 			& li {
 				display: flex;
 				align-items: center;
-				gap: 0.2em;
+				padding: 0 0.5em;
+				transition: background-color 0.2s ease-in-out;
+				&:hover {
+					background-color: var(--gray-50);
+				}
 				& svg {
 					width: 15px;
 					height: 15px;
