19bf6e1f12b566214661c442116f41e518c9639e
Chris <chris@ghostly.io>
2024-01-16 03:12:03 -0500

    chore: 🔧 Add postcss-import and postcss-preset-env plugins to package.json and postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const autoprefixer = require('autoprefixer');
+const atImport = require('postcss-import');
+const postcssPresetEnv = require('postcss-preset-env');
 
 const config = {
-	plugins: [autoprefixer]
+	plugins: [
+		autoprefixer,
+		atImport(),
+		postcssPresetEnv({
+			stage: 3,
+			features: {
+				'nesting-rules': true,
+				'custom-media-queries': true,
+				'media-query-ranges': true
+			}
+		})
+	]
 };
 
 module.exports = config;
